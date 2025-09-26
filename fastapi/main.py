9528ebc4002431b10700da1bd96b59f8d7e8613f
levinlinux <wagner.ulisses@gmail.com>
2025-09-26 00:43:36 -0300

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-from typing import Optional
-
-=======
->>>>>>> b84106d (wagner alteracao fastapi-render)
 from fastapi import FastAPI
 
 app = FastAPI()
 
-<<<<<<< HEAD
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-=======
 @app.get('/')
 def index():
     return {"message": "Funciona"}
->>>>>>> b84106d (wagner alteracao fastapi-render)
+
